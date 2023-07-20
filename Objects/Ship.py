78a5e6e14f1fc61631edb1377454244e671d23c3
redzebra7 <25woodj@mbbc.qld.edu.au>
2023-07-20 12:16:12 +1000

    created spaceship object

---
+++ Objects/Ship.py
@@ -0,0 +1,17 @@
+from GameFrame import RoomObject
+import pygame
+
+class Ship(RoomObject):
+    def __init__(self, room, x, y):
+        RoomObject.__init__(self, room, x, y)
+
+        image = self.load_image("Ship.png")
+        self.set_image(image,100,100)
+
+        self.handle_key_events = True
+
+    def key_pressed(self, key):
+        if key[pygame.K_w]:
+            self.y_speed = -10
+        elif key[pygame.K_s]:
+            self.y_speed = 10
--- Rooms/GamePlay.py
+++ Rooms/GamePlay.py
@@ -1,7 +1,11 @@
 from GameFrame import Level
+from Objects.Ship import Ship
 
 class GamePlay(Level):
     def __init__(self, screen, joysticks):
         Level.__init__(self, screen, joysticks)
 
         self.set_background_image("Background.png")
+
+        self.add_room_object(Ship(self, 25, 50))
+    
