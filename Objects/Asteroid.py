58e98f9b3e3f6f08f14b4b4f1d3b15c33886ce8d
redzebra7 <25woodj@mbbc.qld.edu.au>
2023-07-27 08:14:57 +1000

    Update Asteroid.py

--- Objects/Asteroid.py
+++ Objects/Asteroid.py
@@ -1,4 +1,4 @@
-from GameFrame import RoomObject
+from GameFrame import RoomObject, Globals
 import random
 
 class Asteroid(RoomObject):
@@ -19,3 +19,20 @@ class Asteroid(RoomObject):
 
         angle = random.randint(135,225)
         self.set_direction(angle,10)
+    
+    def step(self):
+        self.keep_in_room()
+        self.outside_of_room()
+
+    def keep_in_room(self):
+        if self.y < 0:
+            self.y = 0
+            self.y_speed *= -1
+        elif self.y > Globals.SCREEN_HEIGHT - self.height:
+            self.y = Globals.SCREEN_HEIGHT - self.height
+            self.y_speed *= -1
+    
+    def outside_of_room(self):
+        if self.x + self.width < 0:
+            print("asteroid deleted")
+            self.room.delete_object(self)
