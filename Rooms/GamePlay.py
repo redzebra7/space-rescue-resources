98d67c89a04d01ce80263fc8e7d03270b314df30
redzebra7 <25woodj@mbbc.qld.edu.au>
2023-08-11 15:33:08 +1000

    Revert "hud"
    This reverts commit 5ddf20e6e8d8afdb9a8799edf71571e7d4acce0a.

--- Rooms/GamePlay.py
+++ Rooms/GamePlay.py
@@ -1,7 +1,6 @@
-from GameFrame import Level, Globals
+from GameFrame import Level
 from Objects.Ship import Ship
 from Objects.Zork import Zork
-from Objects.Hud import Score
 
 class GamePlay(Level):
     def __init__(self, screen, joysticks):
@@ -11,10 +10,4 @@ class GamePlay(Level):
 
         self.add_room_object(Ship(self, 25, 50))
         self.add_room_object(Zork(self, 1120, 50))
-
-        self.score = Score(self,
-                           Globals.SCREEN_WIDTH/2 - 20, 20,
-                           str(Globals.SCORE))
-        self.add_room_object(self.score)
-        
     
